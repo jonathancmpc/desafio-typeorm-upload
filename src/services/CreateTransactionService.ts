35ff45038f2935a82ff3268487407129a1969a4d
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-07-04 12:36:42 -0300

    Insercao no Banco de Dados

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,60 @@
+import { getRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getRepository(Transaction);
+
+    const checkCategoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategoryExists) {
+      const category_insert = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(category_insert);
+
+      const transaction = transactionsRepository.create({
+        title,
+        value,
+        type,
+        category_id: category_insert.id,
+      });
+
+      await transactionsRepository.save(transaction);
+
+      return transaction;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: checkCategoryExists.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
